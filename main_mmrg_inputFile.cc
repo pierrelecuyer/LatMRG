4eeb11adba0e235a5a14de156bbc87f473d5f893
paulwmbrg <paul.wambergue@gmail.com>
2017-07-21 13:47:24 -0400

    adding new input file to test lacunary indices

--- main_mmrg_inputFile.cc
+++ main_mmrg_inputFile.cc
@@ -74,12 +74,15 @@ int main ()
    //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/latZZDD_test1";
    //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/latZZDD_test2";
    
-   string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/mmrg_test1";
+   string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/lacunaryMRG_test1";
+   
+   //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/mmrg_test1";
    //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/mmrg_test2";
    //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/mmrg_test3";
    //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/mmrg_test4";
    //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/mmrg_test5";
    //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/mmrg_test6";
+   
    
    struct stat buf; // properties of a file or directory
    LatTestAll testall;
