4a6dca32679fe94327392d42b2b510967310c46e
paulwmbrg <paul.wambergue@gmail.com>
2017-07-24 13:39:58 -0400

    no message

--- main_mmrg_inputFile.cc
+++ main_mmrg_inputFile.cc
@@ -74,11 +74,11 @@ int main ()
    //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/latZZDD_test1";
    //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/latZZDD_test2";
    
-   string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/lacunaryMRG_test1";
+   //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/lacunaryMRG_test1";
    //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/lacunaryMRG_test2";
    
    //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/mmrg_test1";
-   //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/mmrg_test2";
+   string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/mmrg_test2";
    //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/mmrg_test3";
    //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/mmrg_test4";
    //string testLocation = "/Users/paulwambergue/UdeM/latmrg/inputTestFiles/mmrg_test5";
