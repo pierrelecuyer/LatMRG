1c7ada1f986168f03361d9d6a5a2e9c8b9492804
paulwmbrg <paul.wambergue@gmail.com>
2017-08-22 12:59:52 -0400

    fixing wscript target names causing linking issues when compiling

--- src/IntLattice.cc
+++ src/IntLattice.cc
@@ -240,10 +240,6 @@ Normalizer * IntLattice::getNormalizer (NormaType norma, int alpha, bool dualF)
    else // primal basis
       logDensity = m_order * log(m_modulo);
 
-
-   //PW_TODO: quand dim < order, moins de points que ça
-   
-
    switch (norma) {
    case BESTLAT:
       normal = new NormaBestLat (logDensity, dim);
