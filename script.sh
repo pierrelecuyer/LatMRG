41416b5eacdb42f66c8b1341fdc94845417ef378
Marc-Antoine Savard <ma2112savard@gmail.com>
2019-06-03 11:38:46 -0400

    Adding research comfigurations

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_3_3.dat > 61_3_3.res &
+./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_3_4.dat > 61_3_4.res &
+./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_3_5.dat > 61_3_5.res &
+./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_3_6.dat > 61_3_6.res &
+./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_3_7.dat > 61_3_7.res &
+./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_3_8.dat > 61_3_8.res &
+./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_3_9.dat > 61_3_9.res &
