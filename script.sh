7bfefbfa36f10bb129d186fe53bf53255b12e366
Marc-Antoine Savard <ma2112savard@gmail.com>
2019-06-03 23:46:11 -0400

    Changing test scripts yet again

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_3_3.dat > 61_3_3.res &
-sleep 60
-./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_3_4.dat > 61_3_4.res &
-sleep 60
-./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_3_5.dat > 61_3_5.res &
-sleep 60
-./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_3_6.dat > 61_3_6.res &
-sleep 60
-./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_3_7.dat > 61_3_7.res &
-leep 60
-./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_3_8.dat > 61_3_8.res &
-sleep 60
-./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_3_9.dat > 61_3_9.res &
+./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_4_4.dat > 61_4_4.res &
+sleep 600
+./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_4_5.dat > 61_4_5.res &
+sleep 600
+./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_5_5.dat > 61_5_5.res &
+sleep 600
+./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_5_6.dat > 61_5_6.res &
+sleep 600
+./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_6_6.dat > 61_6_6.res &
+sleep 600
+./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_6_7.dat > 61_6_7.res &
+sleep 600
+./bin/SeekRe examples/inputs/seekre/pow2/pow2_61_full.dat > 61_full.res &
