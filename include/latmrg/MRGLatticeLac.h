228acd002410326ccda726b73851ad4b399a0509
ChrisW84-HRW <83717974+ChrisW84-HRW@users.noreply.github.com>
2024-09-10 12:39:06 +0200

    Polished version

--- include/latmrg/MRGLatticeLac.h
+++ include/latmrg/MRGLatticeLac.h
@@ -1,3 +1,20 @@
+// This file is part of LatMRG.
+//
+// Copyright (C) 2012-2023  The LatMRG authors, under the supervision
+// of Pierre L'Ecuyer at Universit� de Montr�al.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #ifndef LATMRG_MRGLATTICELAC_H
 #define LATMRG_MRGLATTICELAC_H
 
@@ -6,8 +23,6 @@
 #include "latticetester/Types.h"
 #include "latticetester/Lacunary.h"
 #include "latticetester/IntLatticeExt.h"
-//#include "latticetester/Types.h"
-// #include "latticetester/Lacunary.h"
 #include "latticetester/MRGLattice.h"
 #include "latmrg/FlexModInt.h"
 
@@ -48,13 +63,13 @@ public:
     * Copy constructor. The maximal dimension of the new basis is set to
     * <tt>Lat</tt>’s current dimension.
     */
-   // MRGLatticeLac(const MRGLatticeLac &Lat);
+   MRGLatticeLac(const MRGLatticeLac<Int, Real> &Lat);
 
    /**
     * Assigns `Lat` to this object. The maximal dimension of this basis is
     * set to <tt>Lat</tt>’s current dimension.
     */
-   // MRGLatticeLac& operator=(const MRGLatticeLac &Lat);
+   MRGLatticeLac& operator=(const MRGLatticeLac<Int, Real> &Lat);
 
    /**
     * Destructor.
@@ -65,11 +80,6 @@ public:
     * Sets the lacunary indices for this lattice to `lac`.
     */
    void setLac(const IntVec &lac) {m_lac = lac;};
-   
-   /**
-    * Maybe this function does not need to be changed.
-    */
-   bool buildProjection0(IntMat &basis, int64_t dimbasis, IntMat &pbasis, const Coordinates &proj) override;
 
    /**
     * Returns the \f$j\f$-th lacunary index.
@@ -85,8 +95,6 @@ public:
 
 protected:
 
-   // void initStates();
-   
    /**
     * This function overrides the correpsonding protected function in 'MRGLattice'.
     * It Builds a basis directly in `d` dimensions, as explained in Section 4.1.9 of
@@ -112,12 +120,10 @@ protected:
     * We can redefine  `setaa` so it computes and set it.
     * We can also compute it and set it in `buildBasis0`.  Probably safer.  ******
     */
-   typename FlexModInt<Int>::PolX m_Pz;   // Maybe not needed.
-   
-   
-
+   typename FlexModInt<Int>::PolX m_Pz;   // Maybe not needed.  
 
 };
+
 
 //===========================================================================
 // Implementation:
@@ -130,6 +136,46 @@ MRGLatticeLac<Int, Real>::MRGLatticeLac(const Int &m, const IntVec &aa, int64_t 
       setLac(lac);
       ZZ_p::init(m);
 }
+
+
+//============================================================================
+
+// Copy Constructor
+template<typename Int, typename Real>
+MRGLatticeLac<Int,Real>::MRGLatticeLac(const MRGLatticeLac<Int, Real> &Lat) { 
+   this->m_modulo = Lat.m_modulo;
+   this->m_maxDim = Lat.m_maxDim;
+   this->m_dim = Lat.m_dim;
+   this->m_dimdual = Lat.m_dimdual;
+   this->m_norm = Lat.m_norm;
+   this->m_vecNorm = RealVec(Lat.m_vecNorm);
+   this->m_dualvecNorm = RealVec(Lat.m_dualvecNorm);
+   this->m_basis = Lat.m_basis;
+   this->m_dualbasis = Lat.m_dualbasis;   
+   setLac(Lat.m_lac);
+}
+
+
+//============================================================================
+
+// Copy Constructor
+template<typename Int, typename Real>
+    MRGLatticeLac<Int, Real> & MRGLatticeLac<Int, Real>::operator= (const MRGLatticeLac<Int, Real> & Lat) {
+    this->m_modulo = Lat.m_modulo;
+   this->m_maxDim = Lat.m_maxDim;
+   this->m_dim = Lat.m_dim;
+   this->m_dimdual = Lat.m_dimdual;
+   this->m_norm = Lat.m_norm;
+   this->m_vecNorm = RealVec(Lat.m_vecNorm);
+   this->m_dualvecNorm = RealVec(Lat.m_dualvecNorm);
+   this->m_basis = Lat.m_basis;
+   this->m_dualbasis = Lat.m_dualbasis;   
+   setLac(Lat.m_lac);
+   return *this;
+}
+
+
+//============================================================================
 
 // This applies phi inverse as described in the guide, and reverses the coordinates.
 template<typename Int, typename Real>
@@ -203,11 +249,10 @@ void MRGLatticeLac<Int, Real>::buildBasis0(IntMat &basis, int64_t d) {
 }
 
 
-//=========================================x===================================
+//============================================================================
 
 // Increases the dimension of given basis from d-1 to d dimensions.
 // We compute one new column using the polynomial representation.
-
 
 template<typename Int, typename Real>
 void MRGLatticeLac<Int, Real>::incDimBasis0(IntMat &basis, int64_t d) {
@@ -233,204 +278,5 @@ void MRGLatticeLac<Int, Real>::incDimBasis0(IntMat &basis, int64_t d) {
       basis[i][d-1] = (i == d-1) * this->m_modulo;
 }
 
-
-
-//============================================================================
-
-// We must be able to do this with basis equal to either m_basis or m_basis0.
-// We use the columns of basis to construct generating vectors and a pbasis for the projection.
-// This function returns the value of `projCase`, which is `true` iff the first m_order coordinates
-// are all in the projection.
-
-// This one may still work with no change. Check carefully and adjust if needed.      ************
-//
-
-template<typename Int, typename Real>
-bool MRGLatticeLac<Int, Real>::buildProjection0(IntMat &basis, int64_t dimbasis, IntMat &pbasis,
-      const Coordinates &proj) {
-   int64_t d = proj.size();
-   int64_t i, j;
-   // projCase1 = false;
-   // Check if we are in case 1.
-   // This assumes that the coordinates of each projection are always in increasing order!  ***
-   bool projCase1 = true; // This holds if the first m_order coordinates are all in `proj`.
-   if (d < (unsigned) this->m_order) projCase1 = false;
-   else {
-      j = 0;
-      for (auto it = proj.begin(); it != proj.end(); it++, j++) {
-         if (j < this->m_order) {
-            if (*it != unsigned(j + 1)) projCase1 = false;
-         } else break;
-      }
-   }
-   if (projCase1) {
-      // We first compute the first m_order rows of the projection basis.
-      for (i = 0; i < this->m_order; i++) {
-         j = 0;
-         for (auto it = proj.begin(); it != proj.end(); it++, j++)
-            pbasis[i][j] = basis[i][*it - 1];
-      }
-      // Then the other rows.
-      for (i = this->m_order; i < d; i++)
-         for (j = 0; j < d; j++)
-            pbasis[i][j] = this->m_modulo * (i == j);
-   } else {
-      // In this case we need to use the more general algorithm.
-      j = 0;
-      for (auto it = proj.begin(); it != proj.end(); it++, j++) {
-         // Set column j of all generating vectors, for (j+1)-th coordinate of proj.
-         for (i = 0; i < dimbasis; i++)
-            this->m_genTemp[i][j] = basis[i][*it - 1];
-      }
-      // std::cout << " Generating vectors: \n" << m_genTemp << "\n";
-      upperTriangularBasis(this->m_genTemp, pbasis, this->m_modulo, dimbasis, d);
-   }
-   return projCase1;
-}
-
-
-// Check if any other function in the parent `MRGLattice`  needs to be changed.      ************
-
-//============================================================================
-
-// The old implementation.  Will be removed.                *********
-//
-/*
-template<typename Int, typename Real>
-void MRGLattice<Int, Real>::buildLaBasis(int64_t d) {
-
-   if (this->m_order > ORDERMAX)
-      LatticeTester::MyExit(1, "MRGLattice::buildLaBasis:   k > ORDERMAX");
-
-   initStates();
-   int64_t IMax = m_lac.getSize();
-
-   IntVec b;
-   b.SetLength(this->m_order + 1);
-   LatticeTester::Invert(m_aCoeff, b, this->m_order);
-
-   // b is the characteristic polynomial of the MRG.
-   PolyPE < Int > ::setModulus(this->m_modulo);
-   PolyPE < Int > ::setPoly(b);
-   PolyPE<Int> pol;
-   int64_t ord = 0;
-
-   // Construction d'un systeme generateur modulo m.
-   for (int64_t k = 0; k < IMax; k++) {
-      // pour chaque indice lacunaire
-      NTL::conv(m_e, m_lac[k]);
-
-      // x^m_e Mod f(x) Mod m
-      pol.powerMod(m_e);
-      pol.toVector(m_xi);
-
-      ord = 0;
-      for (int64_t i = 1; i <= this->m_order; i++) {
-         if (m_ip[i]) {
-            ++ord;
-            m_t5 = 0;
-            for (int64_t j = 1; j <= this->m_order; j++)
-               m_t5 += m_sta[i][j] * m_xi[j - 1];
-            this->m_wSI[ord][k] = m_t5;
-         }
-      }
-   }
-
-   //  From here we can use BasisConstruction.  *********
-*/
-
-   /* On veut s'assurer que la base m_v soit triangulaire (pour satisfaire
-    * les conditions de l'article \cite{rLEC94e} [sec. 3, conditions sur
-    * V_i >= i]) et de plein rang (on remplace les lignes = 0 par lignes
-    * avec m sur la diagonale).
-    * */
-/*
-   LatticeTester::Triangularization<IntMat>(this->m_wSI, this->m_vSI, ord, IMax, this->m_modulo);
-   LatticeTester::CalcDual < IntMat > (this->m_vSI, this->m_wSI, IMax, this->m_modulo);
-
-   // Construire la base de dimension 1
-   this->m_basis[0][0] = this->m_vSI[0][0];
-   this->m_dualbasis[0][0] = this->m_wSI[0][0];
-   this->setDim(1);
-
-   this->setNegativeNorm();
-   this->setDualNegativeNorm();
-
-   //  This approach could be slow!  *****
-   for (int64_t i = 2; i <= d; i++)
-      incDimLaBasis(IMax);
-
-   // for debugging
-   // trace("ESPION_1", 1);
-}
-*/
-
-//============================================================================
-/*
-template<typename Int, typename Real>
-void MRGLattice<Int, Real>::incDimLaBasis(int64_t IMax) {
-
-   LatticeTester::IntLatticeExt<Int, Real>::incDim();
-   const int64_t dim = this->getDim(); // new dimension (dim++)
-*/
-   /*
-    if (dim >= IMax) {
-    MyExit (0,
-    "Dimension of the basis is too big:\nDim > Number of lacunary indices.");
-    }
-    */
-/*
-   IntVec tempLineBasis(dim);
-   IntVec tempColBasis(dim);
-
-   for (int64_t i = 0; i < dim - 1; i++) {
-
-      // tempLineBasis <- m_basis[i]
-      for (int64_t k = 0; k < dim - 1; k++)
-         tempLineBasis[k] = this->m_basis[i][k];
-
-      for (int64_t i1 = 0; i1 < dim - 1; i1++) {
-
-         Int tempScalDual;
-         LatticeTester::ProdScal<Int>(tempLineBasis, this->m_wSI[i1], dim, tempScalDual);
-         LatticeTester::Quotient(tempScalDual, this->m_modulo, tempScalDual);
-         this->m_t1 = tempScalDual * this->m_vSI[i1][dim - 1];
-         tempColBasis[i] += this->m_t1;
-      }
-      LatticeTester::Modulo(tempColBasis[i], this->m_modulo, tempColBasis[i]);
-      this->m_basis[i][dim - 1] = tempColBasis[i];
-   }
-
-   for (int64_t j = 0; j < dim - 1; j++)
-      this->m_basis[dim - 1][j] = 0;
-   this->m_basis[dim - 1][dim - 1] = this->m_vSI[dim - 1][dim - 1];
-
-   for (int64_t i = 0; i < dim - 1; i++)
-      this->m_dualbasis[i][dim - 1] = 0;
-
-   for (int64_t j = 0; j < dim - 1; j++) {
-
-      Int tempScalDualBis;
-
-      for (int64_t i = 0; i < dim - 1; i++) {
-         this->m_t1 = this->m_dualbasis[i][j];
-         this->m_t1 *= tempColBasis[i];
-         tempScalDualBis += this->m_t1;
-      }
-      if (tempScalDualBis != 0) tempScalDualBis = -tempScalDualBis;
-
-      LatticeTester::Quotient(tempScalDualBis, this->m_vSI[dim - 1][dim - 1], tempScalDualBis);
-      this->m_dualbasis[dim - 1][j] = tempScalDualBis;
-   }
-
-   LatticeTester::Quotient(this->m_modulo, this->m_vSI[dim - 1][dim - 1], this->m_t1);
-   this->m_dualbasis[dim - 1][dim - 1] = this->m_t1;
-
-   this->setNegativeNorm();
-   this->setDualNegativeNorm();
-
-}
-*/
 }
 #endif
-
