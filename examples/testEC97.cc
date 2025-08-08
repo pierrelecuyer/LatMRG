3b55586d2ede9abf975ebc97f8f7ad513c241caf
ChrisW84-HRW <83717974+ChrisW84-HRW@users.noreply.github.com>
2025-08-08 09:24:18 +0200

    Update testEC97.cc

--- examples/testEC97.cc
+++ examples/testEC97.cc
@@ -23,46 +23,40 @@
 
 using namespace LatticeTester;
 
+template<typename Int, typename Real>
+static void FOMLattice(Int m, int64_t maxdim, const NTL::Vec<Int> a, const NTL::Vec<int64_t> t) {
+  LatMRG::MRGLattice<NTL::ZZ, double> lat(m, a, maxdim);
+  lat.setUsePolynomialBasis(true);
+  ReducerBB<NTL::ZZ, double> m_red(lat); 
+  WeightsUniform weights(1.0);
+  NormaBestLat normaDual(-log(m), a.length()-1, maxdim, L2NORM);
+  FigureOfMeritDualM<NTL::ZZ, double> fomdual(t, weights, normaDual, &m_red);
+  fomdual.setVerbosity(2);
+   IntLattice<NTL::ZZ, double> proj(m, t.length());
+  fomdual.computeMerit(lat, proj);
+}
+
+
 int main() {
 
-   int64_t maxdim = 8;
-   
+   int64_t maxdim = 8;   
    std::cout << "Types: NTL::ZZ, double \n";
-
    NTL::Vec<int64_t> t; // The t-vector for the FOM.
    t.SetLength(2);
    t[0] = 8;    // We look at successive coordinates in up to t[0] dimensions.
-   t[1] = 0;    // Then pairs and triples up to coord. 5.
-   
+   t[1] = 0;    // Then pairs and triples up to coord. 5.   
    NTL::ZZ m;
-   m = 2147483647;
-
    NTL::Vec<NTL::ZZ> a; // Vector a has size 4, a[j] contains a_j.
+
+   
+   std::cout << "\n=============================================================\n";
+   std::cout << "Results for a MRG example with m = 2147483647 and a = 45991\n";
+   m = 2147483647;
    a.SetLength(2);
    a[1] = 45991;
-    
-   std::cout << "\n=============================================================\n";
-     std::cout << "Results for a MRG example with m = 2147483647 and a = 45991\n";
-
-   LatMRG::MRGLattice<NTL::ZZ, double> lat(m, a, maxdim);
-
-   lat.setUsePolynomialBasis(true);
-
-   ReducerBB<NTL::ZZ, double> m_red(lat); 
-
-   WeightsUniform weights(1.0);
-
-   NormaBestLat normaDual(-log(m), a.length()-1, maxdim, L2NORM);
-
-   FigureOfMeritDualM<NTL::ZZ, double> fomdual(t, weights, normaDual, &m_red);
-
-   IntLattice<NTL::ZZ, double> proj(m, t.length());
-
-   fomdual.setVerbosity(2);
-
-   fomdual.computeMerit(lat, proj);
-
+   FOMLattice<NTL::ZZ, double>(m, maxdim, a, t);   
    std::cout << "\n The results are the same as in [EC97], Table 6!\n";
+
 
    std::cout << "\n=============================================================\n";
    std::cout << "Results for a MRG example with m = 9223372036854773561, a = (1145902849652723, 0, -1184153554609676). \n";
@@ -71,54 +65,18 @@ int main() {
    a[1] = 1145902849652723;
    a[2] = 0;
    a[3] = -1184153554609676;
-
-   LatMRG::MRGLattice<NTL::ZZ, double> lat2(m, a, maxdim);
-
-   lat2.setUsePolynomialBasis(true);
-
-   ReducerBB<NTL::ZZ, double> m_red2(lat2); 
-
-   NormaBestLat normaDual2(-log(m), a.length()-1, maxdim);
-
-   FigureOfMeritDualM<NTL::ZZ, double> fomdual2(t, weights, normaDual2, &m_red2);
-   
-   IntLattice<NTL::ZZ, double> proj2(m, t.length());
-
-   fomdual2.setVerbosity(2);
-
-   fomdual2.computeMerit(lat2, proj2);
-
-   std::cout << "\n The results are the same as in [EC97], Table 7!\n";
+  FOMLattice<NTL::ZZ, double>(m, maxdim, a, t);
+  std::cout << "\n The results are the same as in [EC97], Table 7!\n";
 
    
    std::cout << "\n=============================================================\n";
    std::cout << "Results for a MRG example with m = 4294967296, a = 1099087573. \n";
+   maxdim = 8;
    m = 4294967296;
+   m = m / 4;
    a.SetLength(2);
-   a[1] = 1099087573;
-
-   t.SetLength(1);
-   t[0] = 8;    // We look at successive coordinates in up to t[0] dimensions.
-
-    LatMRG::MRGLattice<NTL::ZZ, double> lat3(m, a, maxdim);
-
-   lat3.setUsePolynomialBasis(true);
-
-   ReducerBB<NTL::ZZ, double> m_red3(lat3); 
-
-   NormaBestLat normaDual3(-log(m), a.length()-1, maxdim);
-
-   FigureOfMeritDualM<NTL::ZZ, double> fomdual3(t, weights, normaDual3, &m_red3);
-   
-   IntLattice<NTL::ZZ, double> proj3(m, t.length());
-
-   fomdual3.setVerbosity(2);
-
-   fomdual3.computeMerit(lat3, proj3);
-
-   std::cout << "\n The results are NOT the same as in [EC97], Table 1!\n";
-
-
-   
+   a[1] = 1099087573;  
+   FOMLattice<NTL::ZZ, double>(m, maxdim, a, t);    
+  std::cout << "\n The results are the same as in [EC97], Table 1!\n";
 
 }
