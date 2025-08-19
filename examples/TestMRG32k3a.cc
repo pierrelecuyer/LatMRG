835bd74ac97da8dcdd7a99aa8be1e3efa273d4d9
pierre <lecuyer@iro.umontreal.ca>
2025-08-18 20:55:50 -0400

    Improved TestMRG32k3a.cc

--- examples/TestMRG32k3a.cc
+++ examples/TestMRG32k3a.cc
@@ -3,7 +3,7 @@
  */
 
 // This defines the Int type. We must recompile to change it.
-#define TYPES_CODE  ZD     // Int = ZZ, Real = double
+#define TYPES_CODE  ZQ     // Int = ZZ, Real = double
 
 #include <NTL/ZZ.h>
 #include "latticetester/FlexTypes.h"
@@ -28,95 +28,73 @@ int main() {
    aa[1] = to_ZZ("18169668471252892557");
    aa[2] = to_ZZ("3186860506199273833");
    aa[3] = to_ZZ("8738613264398222622");
-   Chrono timer;
 
-   int64_t maxdim(45);  // Maximum dimension of the lattice
+   int64_t maxdim(50);  // Maximum dimension of the lattice
    NTL::Vec <int64_t> t; // The t-vector for the FOM.
    t.SetLength(3);
    t[0] = maxdim;  t[1] = 0;  t[2] = 0;
-   double merit, minmerit;
    MRGLattice<Int, Real> mrgc = MRGLattice<Int, Real>(m, aa, maxdim);
    WeightsUniform weights(1.0);
    NormaRogers normaDual(-log(m), 3, maxdim);  // Normalization for m-dual: Roger's bounds as in rLEC99b.
    ReducerBB<Int, Real> red(maxdim);   // Single ReducerBB with internal lattice `lat`.
    FigureOfMeritDualM<Int, Real> fom(t, weights, normaDual, &red, true); // FoM for dual lattice.
    fom.setVerbosity(3);
-   std::cout << "\nResults from TestMRG32k3a.cc \n\n";
+   std::cout << "\nResults from TestMRG32k3a.cc \n";
 
    // We first compute the FOM in successive dimensions up to maxdim, with the default BKZ + BB.
    // It uses delta = 0.99999 and blocksize = 10.
-   std::cout << "=============================================\n";
+   std::cout << "\n=============================================\n";
    std::cout << "MRG32k3a, with BKZ+BB with default parameters \n";
-   timer.init();
-   mrgc.buildDualBasis(4);
-   minmerit = fom.computeMeritSucc(mrgc);
-   std::cout << "minmerit = " << minmerit << "\n";
-   std::cout << "CPU time: " << timer.toString() << "\n\n";
+   fom.computeMeritSucc(mrgc);
 
-   // Here we apply only the default BKZ, no BB.
+   // Again BKZ + BB, but with delta = 0.99999 and blocksize k = 20.
+   fom.setBKZ(0.99999, 20);
+   std::cout << "\n=============================================\n";
+   std::cout << "MRG32k3a, with BKZ+BB with delta = 0.99999 and block size = 20 \n";
+   fom.computeMeritSucc(mrgc);
+
+   // Again BKZ + BB, but with delta = 0.99999999 and blocksize k = 50.
+   fom.setBKZ(0.99999999, 50);
+   std::cout << "\n=============================================\n";
+   std::cout << "MRG32k3a, with BKZ+BB with delta = 0.99999999 and block size = 50 \n";
+   fom.computeMeritSucc(mrgc);
+
+   // Here we apply only LLL with delta = 0.9, no BB.
    fom.setBB(false);
-   std::cout << "===========================================\n";
-   std::cout << "MRG32k3a, only BKZ, no BB \n";
-   timer.init();
-   mrgc.buildDualBasis(4);
-   minmerit = fom.computeMeritSucc(mrgc);
-   std::cout << "minmerit = " << minmerit << "\n";
-   std::cout << "CPU time: " << timer.toString() << "\n\n";
-
-   // Then only LLL with delta = 0.9.
    fom.setBKZ(0.0);
    fom.setLLL(0.9);
-   std::cout << "===========================================\n";
+   std::cout << "\n===========================================\n";
    std::cout << "MRG32k3a, only LLL with delta = 0.9, no BB \n";
-   timer.init();
-   mrgc.buildDualBasis(4);
-   minmerit = fom.computeMeritSucc(mrgc);
-   std::cout << "minmerit = " << minmerit << "\n";
-   std::cout << "CPU time: " << timer.toString() << "\n\n";
+   fom.computeMeritSucc(mrgc);
+
+   // Here we apply only the default BKZ, no BB.
+   fom.setBKZ(0.99999);
+   std::cout << "\n===========================================\n";
+   std::cout << "MRG32k3a, only BKZ with block size k = 10, no BB \n";
+   fom.computeMeritSucc(mrgc);
+
+   // Here we apply only BKZ, but with delta = 0.99999999 and blocksize k = 50.
+   fom.setBKZ(0.99999999, 50);
+   std::cout << "\n===========================================\n";
+   std::cout << "MRG32k3a, only BKZ with delta = 0.99999999 and k = 50, no BB \n";
+   fom.computeMeritSucc(mrgc);
 
    // Here we rebuild the basis from scratch each time we increase the dimension,
    // with the default BKZ + BB.
-   int64_t j;
-   fom.setLLL(0.99999);
+   fom.setLLL(0.0);
    fom.setBKZ(0.99999, 10);
    fom.setBB(true);
-   Coordinates coord;
-   minmerit = DBL_MAX;
-   std::cout << "===========================================\n";
-   std::cout << "MRG32k3a, BKZ+BB, rebuild basis at each step \n";
-   std::cout << "coordinates      sqlen       merit           minmerit \n";
-   timer.init();
-   for (j = 1; j <= 3; j++) coord.insert(j);
-   for (j = 4; j <= maxdim; j++) {
-      coord.insert(j);
-      mrgc.buildDualBasis(j);
-      mrgc.dualize();
-      merit = fom.computeMeritOneProj(mrgc, coord, minmerit);
-      minmerit = min(merit, minmerit);
-   }
-   std::cout << "minmerit = " << minmerit << "\n";
-   std::cout << "CPU time: " << timer.toString() << "\n\n";
+   std::cout << "\n===========================================\n";
+   std::cout << "MRG32k3a, default BKZ+BB, rebuild basis at each step \n";
+   fom.computeMeritSuccRebuild(mrgc);
 
    // Rebuild basis from scratch and use only LLL with delta = 0.9.
    fom.setLLL(0.9);
    fom.setBKZ(0.0);
    fom.setBB(false);
-   minmerit = DBL_MAX;
-   coord.clear();
-   std::cout << "===========================================\n";
+   std::cout << "\n===========================================\n";
    std::cout << "MRG32k3a, only LLL with delta = 0.9, rebuild basis at each step \n";
-   std::cout << "coordinates      sqlen       merit           minmerit \n";
-   timer.init();
-   for (j = 1; j <= 3; j++) coord.insert(j);
-   for (j = 4; j <= maxdim; j++) {
-      coord.insert(j);
-      mrgc.buildDualBasis(j);
-      mrgc.dualize();
-      merit = fom.computeMeritOneProj(mrgc, coord, minmerit);
-      minmerit = min(merit, minmerit);
-   }
-   std::cout << "minmerit = " << minmerit << "\n";
-   std::cout << "CPU time: " << timer.toString() << "\n\n";
+   fom.computeMeritSuccRebuild(mrgc);
 
    return 0;
 }
